366f82fa98e2c0a847e4bcaab117db69d4af1118
Explorer1905 <chavanshravani927@gmail.com>
2026-04-04 20:45:05 +0530

    feat: setup React Router with all page routes

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Home from './pages/Home'
+import Statistics from './pages/Statistics'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/statistics" element={<Statistics />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
